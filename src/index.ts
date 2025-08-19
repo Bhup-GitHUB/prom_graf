01b5b5fba2be92de12e4124f8d408da16f0d2390
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-08-19 14:25:10 +0530

    just added middlware

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,19 @@
 const express = require("express");
 const app = express();
 import { Request, Response } from "express";
+import { middleware } from "./middleware/middleware";
 
-app.get("/get", (req: Request, res: Response) => {
-  const startTime = Date.now();
+// app.use(middleware);
+
+app.get("/get", middleware, (req: Request, res: Response) => {
   res.json("get request");
-
-  const endtime = Date.now();
-  var timeTaken = endtime - startTime;
-  console.log("request on /get took " + timeTaken + "ms");
 });
 
-app.post("/post", (req: Request, res: Response) => {
+app.post("/post", middleware, (req: Request, res: Response) => {
   res.json("post");
 });
 
-app.get("/cpu", (req: Request, res: Response) => {
-  const startTIme = Date.now();
-
+app.get("/cpu", middleware, (req: Request, res: Response) => {
   for (let i = 0; i < 100000000; i++) {
     Math.random();
   }
@@ -25,11 +21,6 @@ app.get("/cpu", (req: Request, res: Response) => {
   res.json({
     "return ": "cpu endpoint",
   });
-
-  const endTime = Date.now();
-  var timeTiaken = endTime - startTIme;
-
-  console.log("request on /cpu took " + timeTiaken + "ms");
 });
 
 app.listen(3000);
